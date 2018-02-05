c823603c22f802a7cbfa6b61498abb71c25ce38b
Thejas-1 <naiktheju@gmail.com>
2018-02-06 01:09:10 +0530

    Added a model

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -4,3 +4,9 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    index = models.AutoField(primary_key=True)
+    food1 = "food1"
+    food2 = "food2"
+    food3 = "food3"
